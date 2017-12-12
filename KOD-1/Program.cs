24f3dd7281803732a4f41245143b6e9eb327e2ef
Benas Bagdanovas <benas.bagdanovas@gmail.com>
2017-12-12 14:57:52 +0200

    Rewritten and redocument most of the 'MatrixG' functions due to the shifted mentality, that a 2D matrix's first element should be a row and not a column. 'MatrixG' can now generate random matrices.

--- KOD-1/Program.cs
+++ KOD-1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using Logic;
 
 namespace KOD_1
@@ -7,173 +8,190 @@ namespace KOD_1
 	public class Program
 	{
 		public static void Main(string[] args)
-		{
-			Test1();
-			Test2();
+		{	
+			Test0(); // Test the creation of 'matrixG'.
+			//Test1(); // Test syndromes.
+			//Test2(); // Test encoding and decoding.
 
 			Console.ReadKey();
 		}
 
-		private static void Test1()
-		{
-			Console.WriteLine();
-			ConsoleHelper.WriteInformation($"--- Started testing in {nameof(Test1)}.");
+		//private static void Test1()
+		//{
+		//	Console.WriteLine();
+		//	ConsoleHelper.WriteInformation($"--- Started testing in {nameof(Test1)}.");
 
-			var results = new List<string>();
-			var supposed = new List<string>();
+		//	var results = new List<string>();
+		//	var supposed = new List<string>();
 
-			// new G = 1 0 1 1 0
-			//         0 1 0 1 1
-			//
-			var matrix = new int[5][];
-			matrix[0] = new int[2] { 1, 0 };
-			matrix[1] = new int[2] { 0, 1 };
-			matrix[2] = new int[2] { 1, 0 };
-			matrix[3] = new int[2] { 1, 1 };
-			matrix[4] = new int[2] { 0, 1 };
+		//	// new G = 1 0 1 1 0
+		//	//         0 1 0 1 1
+		//	//
+		//	var matrix = new int[5][];
+		//	matrix[0] = new int[2] { 1, 0 };
+		//	matrix[1] = new int[2] { 0, 1 };
+		//	matrix[2] = new int[2] { 1, 0 };
+		//	matrix[3] = new int[2] { 1, 1 };
+		//	matrix[4] = new int[2] { 0, 1 };
 
-			var matrixG = new MatrixG(matrix);
-			//matrixG.Display();
+		//	var matrixG = new MatrixG(matrix);
+		//	//matrixG.Display();
 
-			// H = 1 0 1 0 0
-			//     1 1 0 1 0
-			//     0 1 0 0 1
-			var newMatrixH = matrixG.GetMatrixH();
-			//newMatrixH.Display();
+		//	// H = 1 0 1 0 0
+		//	//     1 1 0 1 0
+		//	//     0 1 0 0 1
+		//	var newMatrixH = matrixG.GetMatrixH();
+		//	//newMatrixH.Display();
 
-			// Test leaders = 0 0 0 0 0
-			//                0 0 0 0 1
-			//                0 0 0 1 0
-			//                0 0 1 0 0
-			//                0 1 0 0 0
-			//                1 0 0 0 0
-			//                0 1 1 0 0
-			var leader1 = new int[5] { 0, 0, 0, 0, 0 };
-			var leader2 = new int[5] { 0, 0, 0, 0, 1 };
-			var leader3 = new int[5] { 0, 0, 0, 1, 0 };
-			var leader4 = new int[5] { 0, 0, 1, 0, 0 };
-			var leader5 = new int[5] { 0, 1, 0, 0, 0 };
-			var leader6 = new int[5] { 1, 0, 0, 0, 0 };
-			var leader7 = new int[5] { 0, 1, 1, 0, 0 };
-			var leader8 = new int[5] { 1, 1, 0, 0, 0 };
+		//	// Test leaders = 0 0 0 0 0
+		//	//                0 0 0 0 1
+		//	//                0 0 0 1 0
+		//	//                0 0 1 0 0
+		//	//                0 1 0 0 0
+		//	//                1 0 0 0 0
+		//	//                0 1 1 0 0
+		//	var leader1 = new int[5] { 0, 0, 0, 0, 0 };
+		//	var leader2 = new int[5] { 0, 0, 0, 0, 1 };
+		//	var leader3 = new int[5] { 0, 0, 0, 1, 0 };
+		//	var leader4 = new int[5] { 0, 0, 1, 0, 0 };
+		//	var leader5 = new int[5] { 0, 1, 0, 0, 0 };
+		//	var leader6 = new int[5] { 1, 0, 0, 0, 0 };
+		//	var leader7 = new int[5] { 0, 1, 1, 0, 0 };
+		//	var leader8 = new int[5] { 1, 1, 0, 0, 0 };
 
 
 
-			// Output should be
-			// Syndromes = 0 0 0
-			//			   0 0 1
-			//			   0 1 0
-			//			   1 0 0
-			//			   0 1 1
-			//			   1 1 0
-			//			   1 1 1
-			//			   1 0 1
-			var syndrome1 = newMatrixH.CalculateSyndrome(leader1);
-			var syndrome2 = newMatrixH.CalculateSyndrome(leader2);
-			var syndrome3 = newMatrixH.CalculateSyndrome(leader3);
-			var syndrome4 = newMatrixH.CalculateSyndrome(leader4);
-			var syndrome5 = newMatrixH.CalculateSyndrome(leader5);
-			var syndrome6 = newMatrixH.CalculateSyndrome(leader6);
-			var syndrome7 = newMatrixH.CalculateSyndrome(leader7);
-			var syndrome8 = newMatrixH.CalculateSyndrome(leader8);
+		//	// Output should be
+		//	// Syndromes = 0 0 0
+		//	//			   0 0 1
+		//	//			   0 1 0
+		//	//			   1 0 0
+		//	//			   0 1 1
+		//	//			   1 1 0
+		//	//			   1 1 1
+		//	//			   1 0 1
+		//	var syndrome1 = newMatrixH.CalculateSyndrome(leader1);
+		//	var syndrome2 = newMatrixH.CalculateSyndrome(leader2);
+		//	var syndrome3 = newMatrixH.CalculateSyndrome(leader3);
+		//	var syndrome4 = newMatrixH.CalculateSyndrome(leader4);
+		//	var syndrome5 = newMatrixH.CalculateSyndrome(leader5);
+		//	var syndrome6 = newMatrixH.CalculateSyndrome(leader6);
+		//	var syndrome7 = newMatrixH.CalculateSyndrome(leader7);
+		//	var syndrome8 = newMatrixH.CalculateSyndrome(leader8);
 
-			var result1 = string.Join("", syndrome1);
-			var result2 = string.Join("", syndrome2);
-			var result3 = string.Join("", syndrome3);
-			var result4 = string.Join("", syndrome4);
-			var result5 = string.Join("", syndrome5);
-			var result6 = string.Join("", syndrome6);
-			var result7 = string.Join("", syndrome7);
-			var result8 = string.Join("", syndrome8);
+		//	var result1 = string.Join("", syndrome1);
+		//	var result2 = string.Join("", syndrome2);
+		//	var result3 = string.Join("", syndrome3);
+		//	var result4 = string.Join("", syndrome4);
+		//	var result5 = string.Join("", syndrome5);
+		//	var result6 = string.Join("", syndrome6);
+		//	var result7 = string.Join("", syndrome7);
+		//	var result8 = string.Join("", syndrome8);
 
-			results.Clear();
-			results = new List<string> { result1, result2, result3, result4, result5, result6, result7,result8 };
-			supposed.Clear();
-			supposed = new List<string> { "000", "001" ,"010" ,"100", "011" ,"110" ,"111", "101" };
+		//	results.Clear();
+		//	results = new List<string> { result1, result2, result3, result4, result5, result6, result7,result8 };
+		//	supposed.Clear();
+		//	supposed = new List<string> { "000", "001" ,"010" ,"100", "011" ,"110" ,"111", "101" };
 
-			ConsoleHelper.WriteWarning("Testing syndromes..");
+		//	ConsoleHelper.WriteWarning("Testing syndromes..");
 
-			for (var i = 0; i < results.Count; i++)
-			{
-				if (results[i] != supposed[i])
-					ConsoleHelper.WriteError($"Failed test in {nameof(Test1)} - Syndromes are not calculated properly.");
-			}
+		//	for (var i = 0; i < results.Count; i++)
+		//	{
+		//		if (results[i] != supposed[i])
+		//			ConsoleHelper.WriteError($"Failed test in {nameof(Test1)} - Syndromes are not calculated properly.");
+		//	}
 
-			ConsoleHelper.WriteInformation($"--- Finished testing in {nameof(Test1)}.");
-		}
+		//	ConsoleHelper.WriteInformation($"--- Finished testing in {nameof(Test1)}.");
+		//}
 
-		private static void Test2()
-		{
-			Console.WriteLine();
-			ConsoleHelper.WriteInformation($"--- Started testing in {nameof(Test2)}.");
+		//private static void Test2()
+		//{
+		//	Console.WriteLine();
+		//	ConsoleHelper.WriteInformation($"--- Started testing in {nameof(Test2)}.");
 			
-			var matrix = new int[5][];
-			matrix[0] = new int[2] { 1, 0 };
-			matrix[1] = new int[2] { 0, 1 };
-			matrix[2] = new int[2] { 1, 0 };
-			matrix[3] = new int[2] { 1, 1 };
-			matrix[4] = new int[2] { 0, 1 };
+		//	var matrix = new int[5][];
+		//	matrix[0] = new int[2] { 1, 0 };
+		//	matrix[1] = new int[2] { 0, 1 };
+		//	matrix[2] = new int[2] { 1, 0 };
+		//	matrix[3] = new int[2] { 1, 1 };
+		//	matrix[4] = new int[2] { 0, 1 };
+
+		//	// G = 1 0 1 1 0
+		//	//     0 1 0 1 1
+		//	var matrixG = new MatrixG(matrix);
+		//	//matrixG.Display();
+
+		//	var messageToEncode1 = new int[2] {0,0};
+		//	var messageToEncode2 = new int[2] {1,0};
+		//	var messageToEncode3 = new int[2] {0,1};
+		//	var messageToEncode4 = new int[2] {1,1};
+
+		//	var encodedMessage1 = matrixG.Encode(messageToEncode1);
+		//	var encodedMessage2 = matrixG.Encode(messageToEncode2);
+		//	var encodedMessage3 = matrixG.Encode(messageToEncode3);
+		//	var encodedMessage4 = matrixG.Encode(messageToEncode4);
+
+		//	var encoded1 = string.Join("", encodedMessage1);
+		//	var encoded2 = string.Join("", encodedMessage2);
+		//	var encoded3 = string.Join("", encodedMessage3);
+		//	var encoded4 = string.Join("", encodedMessage4);
+
+		//	var results = new List<string> { encoded1, encoded2, encoded3, encoded4 };
+		//	var supposed = new List<string> { "00000", "10110", "01011", "11101" };
+
+		//	ConsoleHelper.WriteWarning("Testing encoding..");
+
+		//	for (var i = 0; i < results.Count; i++)
+		//	{
+		//		if (results[i] != supposed[i])
+		//			ConsoleHelper.WriteError($"Failed test in {nameof(Test2)} - Messages are not encoded properly.");
+		//	}
+
+
+		//	// H = 1 0 1 0 0
+		//	//     1 1 0 1 0
+		//	//     0 1 0 0 1
+		//	var matrixH = matrixG.GetMatrixH();
+		//	//matrixH.Display();
+
+		//	var decodedMessage1 = matrixH.Decode(encodedMessage1);
+		//	var decodedMessage2 = matrixH.Decode(encodedMessage2);
+		//	var decodedMessage3 = matrixH.Decode(encodedMessage3);
+		//	var decodedMessage4 = matrixH.Decode(encodedMessage4);
+
+		//	var decoded1 = string.Join("", decodedMessage1);
+		//	var decoded2 = string.Join("", decodedMessage2);
+		//	var decoded3 = string.Join("", decodedMessage3);
+		//	var decoded4 = string.Join("", decodedMessage4);
+
+		//	results.Clear();
+		//	results = new List<string>{ decoded1, decoded2, decoded3, decoded4 };
+
+		//	ConsoleHelper.WriteWarning("Testing decoding..");
+
+		//	for (var i = 0; i < results.Count; i++)
+		//	{
+		//		if (results[i] != supposed[i])
+		//			ConsoleHelper.WriteError($"Failed test in {nameof(Test2)} - Messages are not decoded properly.");
+		//	}
+
+		//	ConsoleHelper.WriteInformation($"--- Finished testing in {nameof(Test2)}.");
+		//}
+
+		private static void Test0()
+		{
+			// Todo: patikrinti, kuomet matrica G = (1 1 1).
 
 			// G = 1 0 1 1 0
 			//     0 1 0 1 1
-			var matrixG = new MatrixG(matrix);
-			//matrixG.Display();
 
-			var messageToEncode1 = new int[2] {0,0};
-			var messageToEncode2 = new int[2] {1,0};
-			var messageToEncode3 = new int[2] {0,1};
-			var messageToEncode4 = new int[2] {1,1};
+			var matrix = new int[2][];
+			matrix[0] = new int[5] {1,0,1,1,0};
+			matrix[1] = new int[5] {0,1,0,1,1};
 
-			var encodedMessage1 = matrixG.Encode(messageToEncode1);
-			var encodedMessage2 = matrixG.Encode(messageToEncode2);
-			var encodedMessage3 = matrixG.Encode(messageToEncode3);
-			var encodedMessage4 = matrixG.Encode(messageToEncode4);
-
-			var encoded1 = string.Join("", encodedMessage1);
-			var encoded2 = string.Join("", encodedMessage2);
-			var encoded3 = string.Join("", encodedMessage3);
-			var encoded4 = string.Join("", encodedMessage4);
-
-			var results = new List<string> { encoded1, encoded2, encoded3, encoded4 };
-			var supposed = new List<string> { "00000", "10110", "01011", "11101" };
-
-			ConsoleHelper.WriteWarning("Testing encoding..");
-
-			for (var i = 0; i < results.Count; i++)
-			{
-				if (results[i] != supposed[i])
-					ConsoleHelper.WriteError($"Failed test in {nameof(Test2)} - Messages are not encoded properly.");
-			}
-
-
-			// H = 1 0 1 0 0
-			//     1 1 0 1 0
-			//     0 1 0 0 1
-			var matrixH = matrixG.GetMatrixH();
-			//matrixH.Display();
-
-			var decodedMessage1 = matrixH.Decode(encodedMessage1);
-			var decodedMessage2 = matrixH.Decode(encodedMessage2);
-			var decodedMessage3 = matrixH.Decode(encodedMessage3);
-			var decodedMessage4 = matrixH.Decode(encodedMessage4);
-
-			var decoded1 = string.Join("", decodedMessage1);
-			var decoded2 = string.Join("", decodedMessage2);
-			var decoded3 = string.Join("", decodedMessage3);
-			var decoded4 = string.Join("", decodedMessage4);
-
-			results.Clear();
-			results = new List<string>{ decoded1, decoded2, decoded3, decoded4 };
-
-			ConsoleHelper.WriteWarning("Testing decoding..");
-
-			for (var i = 0; i < results.Count; i++)
-			{
-				if (results[i] != supposed[i])
-					ConsoleHelper.WriteError($"Failed test in {nameof(Test2)} - Messages are not decoded properly.");
-			}
-
-			ConsoleHelper.WriteInformation($"--- Finished testing in {nameof(Test2)}.");
+			var matrixG = new MatrixG(length: matrix[0].GetUpperBound(0) + 1, 
+									  dimension: matrix.GetUpperBound(0) + 1, 
+									  matrix: matrix);
 		}
 	}
 }
--- Logic/MatrixG.cs
+++ Logic/MatrixG.cs
@@ -5,23 +5,182 @@ namespace Logic
 {
 	public class MatrixG
 	{
-		private readonly int[][] _matrix;
+		private readonly int[][] _matrix; // The main matrix.
+		private readonly int _length;	  // Length of the vectors.
+		private readonly int _dimension;  // Number of the vectors.
+
 		private readonly int _rows; // Number of rows in '_matrix'.
 		private readonly int _cols; // Number of columns in '_matrix'.
 		private readonly bool _isTransformableToH;
 
-		// When converting to a standard (H) array.
-		private int _hColFirst;	// Denotes the first column of the H to be matrix.
-		private int _hColLast;  // Denotes the last column of the H to be matrix.
+		// When converting to a standard (H) array (index means value of zero for the first column).
+		private int _indexStandardColFirst; // The index of the column where the standard matrix begins.
+		private int _indexStandardColLast;  // The index of the column where the standard matrix ends.
 
-		public MatrixG(int[][] matrix)
+		/// <summary>
+		/// Returns a new object of type 'MatrixG'.
+		/// </summary>
+		/// <param name="length">Length of the vectors.</param>
+		/// <param name="dimension">Number of vectors.</param>
+		/// <param name="matrix">(Optional) a created matrix to be used. If none is provided - a new one will be generated.</param>
+		public MatrixG(int length, int dimension, int[][] matrix = null)
 		{
-			_matrix = matrix;
-			_rows = _matrix[0].GetUpperBound(0);
-			_cols = _matrix.GetUpperBound(0);
-			_isTransformableToH = IsTransformableToMatrixH();
+			if (length <= 0)
+				throw new ArgumentException("\nThe length cannot be zero or less.");
+
+			if (dimension <= 0)
+				throw new ArgumentException("\nThe dimension cannot be zero or less.");
+
+			_length = length;
+			_dimension = dimension;
+
+			if (matrix != null)
+			{
+				_matrix = CheckIfProperMatrixGiven(matrix) ? matrix : GenerateMatrix(length, dimension);
+			}
 		}
 
+		/// <summary>
+		/// Check if a given matrix is useable.
+		/// </summary>
+		/// <param name="matrix">A matrix to be checked.</param>
+		/// <returns>'true' is the matrix is proper, 'false' if not.</returns>
+		private bool CheckIfProperMatrixGiven(int[][] matrix)
+		{
+			var length = matrix[0].GetUpperBound(0) + 1;
+			var dimension = matrix.GetUpperBound(0) + 1;
+
+			if (length != _length)
+				throw new ArgumentException("\nThe length of vectors in the given matrix " +
+				                            "does not match the length provided to the constructor.");
+
+			if (dimension != _dimension)
+				throw new ArgumentException("\nThe dimension (number of vectors) in the given matrix " +
+				                            "does not match the dimension provided to the constructor.");
+
+			for (var r = 0; r < dimension; r++)
+			{
+				try
+				{
+					var test = matrix[r];
+				}
+				catch (Exception)
+				{
+					throw new ArgumentException("\nThe provided matrix contains a null row.");
+				}
+			}
+
+			if (!IsTransformableToMatrixH(matrix))
+				throw new ArgumentException("\nThe provided matrix is not a standard G matrix " +
+				                            "(can not be transformed to an H matrix).");
+
+			return true;
+		}
+
+		/// <summary>
+		/// Generates a matrix of specified dimensions with a standard matrix in it.
+		/// </summary>
+		/// <param name="length"></param>
+		/// <param name="dimension"></param>
+		/// <returns></returns>
+		private int[][] GenerateMatrix(int length, int dimension)
+		{
+			// Todo: will it work with a 1x1?
+			// Generate the standard matrix.
+			int[][] standardMatrix = GenerateStandardMatrix(dimension);
+			// Generate the random matrix.
+			int[][] randomMatrix = GenerateRandomMatrix(rows: dimension, cols: length - dimension);
+			// Return the combination of them.
+			return CombineMatrices(standardMatrix, randomMatrix);
+		}
+
+		/// <summary>
+		/// Combines two matrices together to form a new one.
+		/// </summary>
+		/// <param name="matrix1">The first matrix.</param>
+		/// <param name="matrix2">The second matrix.</param>
+		/// <returns>A new matrix.</returns>
+		private int[][] CombineMatrices(int[][] matrix1, int[][] matrix2)
+		{
+			if (matrix1.GetUpperBound(0) != matrix2.GetUpperBound(0))
+				throw new ArgumentException("\nThe dimensions of the matrices must be equal!");
+
+			var numberOfRows = matrix1.GetUpperBound(0) + 1;
+			var numberOfCols = matrix1[0].GetUpperBound(0) + 1 + matrix2[0].GetUpperBound(0) + 1;
+			var matrix = new int[numberOfRows][];
+
+			for (var r = 0; r < numberOfRows; r++)
+			{
+				// Create a new row.
+				var row = new int[numberOfCols];
+				// Store the current position when copying values.
+				var colIndex = 0;
+				// Copy values from the first matrix.
+				for (var c1 = 0; c1 <= matrix1[0].GetUpperBound(0); c1++)
+				{
+					row[colIndex] = matrix1[r][c1];
+					colIndex++;
+				}
+				// Copy the values from the second matrix.
+				for (var c2 = 0; c2 <= matrix2[0].GetUpperBound(0); c2++)
+				{
+					row[colIndex] = matrix2[r][c2];
+					colIndex++;
+				}
+				// Add the row to the matrix.
+				matrix[r] = row;
+			}
+
+			return matrix;
+		}
+
+		/// <summary>
+		/// Generates a matrix with random members between '1' and '0' of specified size.
+		/// </summary>
+		/// <param name="rows">Number of rows in the matrix.</param>
+		/// <param name="cols">Number of cols in the matrix.</param>
+		/// <returns>A new matrix.</returns>
+		private int[][] GenerateRandomMatrix(int rows, int cols)
+		{
+			var randomGenerator = new Random(DateTime.Now.Millisecond);
+			var matrix = new int[rows][];
+
+			for (var r = 0; r < rows; r++)
+			{
+				// Create a new row.
+				var vector = new int[cols];
+				// Randomly generate values for it.
+				for (var c = 0; c < cols; c++)
+					vector[c] = randomGenerator.Next(0, 2);
+				// Add the new row to the matrix.
+				matrix[r] = vector;
+			}
+
+			return matrix;
+		}
+
+		/// <summary>
+		/// Generates a new standard matrix.
+		/// </summary>
+		/// <param name="size">What size the matrix should be (example 2x2 would be with 'size' = 2).</param>
+		/// <returns>A new standard matrix.</returns>
+		private int[][] GenerateStandardMatrix(int size)
+		{
+			var matrix = new int[size][];
+			for (var r = 0; r < size; r++)
+			{
+				// Create the new row.
+				var row = new int[size];
+				// Assign the only '1'.
+				row[r] = 1;
+				// Add the row to the matrix.
+				matrix[r] = row;
+			}
+
+			return matrix;
+		}
+
+		// Todo: test.
 		public int[] Encode(int[] vector)
 		{
 			var result = new int[_cols + 1];
@@ -45,36 +204,50 @@ namespace Logic
 		{
 			// Todo: complete this function.
 			// Todo: have a variable for keeping a mapping between words and their encoded variants.
+			return null;
 		}
 
-		public bool IsTransformableToMatrixH()
+		/// <summary>
+		/// Checks to see whether a standard matrix can be found inside.
+		/// </summary>
+		/// <param name="matrix"></param>
+		/// <returns>'true' if can be transformed, 'false' else.</returns>
+		public bool IsTransformableToMatrixH(int[][] matrix)
 		{
-			var location = 0;
-			var answer = false;
+			// Todo: will not work if a given matrix has two columns in a row which can be the start of a standard matrix.
+			var numberOfCols = matrix[0].GetUpperBound(0) + 1;
+			var numberOfRows = matrix.GetUpperBound(0) + 1;
+			// We mark the position in only which a '1' is supposed to be (elsewhere should be '0').
+			var position = 0;
+			var result = false;
 
-			for (var c = 0; c <= _cols; c++)
+			for (var c = 0; c < numberOfCols; c++)
 			{
-				if (ColumnContainsOnlyOne(_matrix[c], location))
+				var column = GetColumn(matrix, c);
+				if (ColumnContainsOnlyOne(column, position))
 				{
-					location++;
-					if (location == _rows + 1)
+					// In the next column the '1' should be a row lower.
+					position++;
+					// When the standard matrix is found.
+					if (position == numberOfRows)
 					{
-						// Used when generating a standard (H) matrix.
-						_hColLast = c; 
-						_hColFirst = c - _rows;
-
-						answer = true;
+						// We use these when generating the H matrix.
+						_indexStandardColFirst = c - numberOfRows + 1; // + 1 because 'numberOfRows' is not an index.
+						_indexStandardColLast = c;
+						result = true;
 						break;
 					}
 				}
 				else
 				{
-					location = 0;
+					// The standard array must always begin with a '1' in the first position (zero index).
+					position = 0;
 				}
 			}
-			return answer;
+			return result;
 		}
 
+		// Todo: test.
 		public MatrixH GetMatrixH()
 		{
 			if (!_isTransformableToH)
@@ -87,29 +260,31 @@ namespace Logic
 			return new MatrixH(matrix);
 		}
 
-		private int[][] CombineMatrices(int[][] standardMatrix, int[][] otherMatrix)
-		{
-			var numberOfColsInStandard = standardMatrix.GetUpperBound(0) + 1;
-			var numberOfColsInOther = otherMatrix.GetUpperBound(0) + 1;
-			var numberOfCols = numberOfColsInStandard + numberOfColsInOther;
-			var matrix = new int[numberOfCols][];
-			var col = 0;
+		//// Todo: test.
+		//private int[][] CombineMatrices(int[][] standardMatrix, int[][] otherMatrix)
+		//{
+		//	var numberOfColsInStandard = standardMatrix.GetUpperBound(0) + 1;
+		//	var numberOfColsInOther = otherMatrix.GetUpperBound(0) + 1;
+		//	var numberOfCols = numberOfColsInStandard + numberOfColsInOther;
+		//	var matrix = new int[numberOfCols][];
+		//	var col = 0;
 
-			for (var c = 0; c < numberOfColsInOther; c++)
-			{
-				matrix[col] = otherMatrix[c];
-				col++;
-			}
+		//	for (var c = 0; c < numberOfColsInOther; c++)
+		//	{
+		//		matrix[col] = otherMatrix[c];
+		//		col++;
+		//	}
 
-			for (var c = 0; c < numberOfColsInStandard; c++)
-			{
-				matrix[col] = standardMatrix[c];
-				col++;
-			}
+		//	for (var c = 0; c < numberOfColsInStandard; c++)
+		//	{
+		//		matrix[col] = standardMatrix[c];
+		//		col++;
+		//	}
 
-			return matrix;
-		}
+		//	return matrix;
+		//}
 
+		// Todo: test.
 		private int[][] CreateStandardMatrix(int dimension)
 		{
 			// Create a matrix with default values.
@@ -126,7 +301,7 @@ namespace Logic
 			return matrix;
 		}
 
-
+		// Todo: test.
 		private int[][] SeparateOtherMatrix()
 		{
 			var numberOfColums = _cols - _rows;
@@ -135,11 +310,11 @@ namespace Logic
 			var col = 0;
 			for (var c = 0; c <= _cols; c++)
 			{
-				if (c < _hColFirst || c > _hColLast)
-				{
-					matrix[col] = _matrix[c];
-					col++;
-				}
+				//if (c < _hColFirst || c > _hColLast)
+				//{
+				//	matrix[col] = _matrix[c];
+				//	col++;
+				//}
 			}
 
 			col = 0;
@@ -164,18 +339,38 @@ namespace Logic
 		}
 
 		/// <summary>
-		/// Returns 'True' if an array of ints contain a '1' in the specified position.
+		/// Checks if an array of ints has only one '1' in it and if it's in the specified position.
 		/// </summary>
 		/// <param name="column">Array of int to search through.</param>
 		/// <param name="position">The position in which a '1' is supposed to be.</param>
-		/// <returns></returns>
+		/// <returns>'true' if in the whole array a '1' is only in the specified position, 'false' otherwise.</returns>
 		private bool ColumnContainsOnlyOne(int[] column, int position)
 		{
+			// Check if the array contains only one '1'.
 			if (column.Count(n => n == 1) != 1)
 				return false;
 
+			// Check if the only '1' is in the specified position.
 			return column[position] == 1;
 		}
+
+		/// <summary>
+		/// Gets a specified column from a specified matrix.
+		/// </summary>
+		/// <param name="matrix">The matrix from which to get the column.</param>
+		/// <param name="columnNumber">Index of which column to get (if you need the first column, you pass a zero).</param>
+		/// <returns></returns>
+		private int[] GetColumn(int[][] matrix, int columnNumber)
+		{
+			var numberOfRows = matrix.GetUpperBound(0) + 1;
+			var column = new int[numberOfRows];
+
+			for (var r = 0; r < numberOfRows; r++)
+				column[r] = matrix[r][columnNumber];
+
+			return column;
+		}
+		
 
 		public void Display()
 		{
